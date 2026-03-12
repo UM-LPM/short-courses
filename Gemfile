7b8f554ded8bd444cbb6df92a00a5bb98793c9f5
cecepasinechka <cvetankapasinecka@gmail.com>
2026-03-12 14:41:40 +0100

    fixed course data displaying

--- Gemfile
+++ Gemfile
@@ -9,3 +9,4 @@ gem "jekyll-paginate-content"
 gem "json"
 gem 'webrick'
 gem "jekyll-datapage-generator"
+gem "dotenv"
--- _plugins/courses/dynamic_courses.rb
+++ _plugins/courses/dynamic_courses.rb
@@ -1,13 +1,57 @@
+require "fileutils"
+require "net/http"
+require "uri"
+require "dotenv/load"
+
 module Jekyll
   class CoursePageGenerator < Jekyll::Generator
     safe true
 
     def generate(site)
-      if site.data["courses"]
-        site.data["courses"].each do |course|
-          site.pages << CoursePage.new(site, site.source, course)
-        end
+      return unless site.data["courses"]
+
+      syllabus_dir = File.join(site.source, "syllabus")
+      FileUtils.mkdir_p(syllabus_dir)
+
+      token = ENV["CMS_API_TOKEN"]
+
+      site.data["courses"].each do |course|
+        download_pdf(course, syllabus_dir, token)
+        site.pages << CoursePage.new(site, site.source, course)
       end
+    end
+
+    private
+
+    def download_pdf(course, syllabus_dir, token)
+      pdf_url = course["pdfUrl"]
+      return if pdf_url.nil? || pdf_url.strip.empty?
+
+      uri = URI.parse(pdf_url)
+
+      filename = "#{course["acronym"]}.pdf"
+      local_path = File.join(syllabus_dir, filename)
+
+      return if File.exist?(local_path)
+
+      http = Net::HTTP.new(uri.host, uri.port)
+      http.use_ssl = uri.scheme == "https"
+
+      request = Net::HTTP::Get.new(uri)
+
+      request["Authorization"] = "#{token}"
+
+      response = http.request(request)
+
+      if response.is_a?(Net::HTTPSuccess)
+        File.binwrite(local_path, response.body)
+        course["localPdfPath"] = "/syllabus/#{filename}"
+        Jekyll.logger.info "PDF:", "Downloaded #{filename}"
+      else
+        Jekyll.logger.warn "PDF:", "Failed #{pdf_url} (#{response.code})"
+      end
+    rescue => e
+      Jekyll.logger.warn "PDF:", e.message
     end
   end
 
